773c5bcad4aac0fab0e885c4ebebc6c430bae44e
Murodjon-Abdulazizov <murodjonabdulazizov2001@gmail.com>
2023-06-12 12:08:22 +0500

    initial commit

---
+++ course.js
@@ -0,0 +1,43 @@
+var formCourse = document.getElementById('myFormCourse')
+
+formCourse.addEventListener('submit', async function(j){
+  j.preventDefault()
+
+  var image_course = document.getElementById('image_course')
+  var science = document.getElementById('science').value
+  var science_text = document.getElementById('science_text').value
+
+  if (!image_course.files[0]) {
+    alert("Please choose an image");
+    return;
+  }
+
+  var file = image_course.files[0];
+  var reader = new FileReader();
+
+  reader.onloadend = async function () {
+    var base64String = reader.result.replace("data:", "").replace(/^.+,/, "");
+    await fetch("http://185.217.131.149:5000/api/v1/course/",{
+      method: 'POST',
+      body:JSON.stringify({
+        image: base64String, 
+        name: science, 
+        description: science_text
+      }), 
+      headers:{
+        "Content-type":"application/json; charset=UTF-8"
+      }
+    })
+    .then(function(responseCourse){
+      return responseCourse.json()
+    })
+    .then(function(data){
+      console.log(data)
+    })
+    .catch((error) => console.error('Error:', error));
+
+    document.getElementById('myFormCourse').reset()  
+  }
+
+  reader.readAsDataURL(file);
+})
---
+++ result.js
@@ -0,0 +1,44 @@
+var formResult = document.getElementById('myFormResult')
+
+formResult.addEventListener('submit', async function(e){
+  e.preventDefault()
+
+  var image_result = document.getElementById('image_result')
+  var result_name = document.getElementById('result_name').value
+  var result_text = document.getElementById('result_text').value
+
+  if (!image_result.files[0]) {
+    alert("Please choose an image");
+    return;
+  }
+
+  var file = image_result.files[0];
+  var reader = new FileReader();
+
+  reader.onloadend = async function () {
+    var base64String = reader.result.replace("data:", "").replace(/^.+,/, "");
+
+    await fetch("http://185.217.131.149:5000/api/v1/result",{
+      method: 'POST',
+      body:JSON.stringify({
+        image: base64String, 
+        fullname: result_name, 
+        description: result_text
+      }), 
+      headers:{
+        "Content-type":"application/json; charset=UTF-8"
+      }
+    })
+    .then(function(responseResult){
+      return responseResult.json()
+    })
+    .then(function(data){
+      console.log(data)
+    })
+    .catch((error) => console.error('Error:', error));
+
+    document.getElementById('myFormResult').reset()  
+  }
+
+  reader.readAsDataURL(file);
+})
